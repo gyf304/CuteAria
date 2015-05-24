62bc9334c1ce2fc106f4efb54496da338d0e1476
Yifan Gu <gyf304@gmail.com>
2015-05-24 00:29:07 -0400

    1.1a
    Added WebSocket RPC using StarScream.
    Added download speed indicator.
    Added download complete notification.
    Fixed a bug that prevents the program from starting properly after a
    force quit.
    Change license to GPLv2.

--- Aria2Helper/AppDelegate.swift
+++ Aria2Helper/AppDelegate.swift
@@ -14,7 +14,7 @@ import Foundation
 import Darwin
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, WebSocketDelegate, NSUserNotificationCenterDelegate {
     
     var aria2cPath = NSBundle.mainBundle().pathForResource("aria2c", ofType: "bin")
     var aria2cTask = NSTask()
@@ -23,20 +23,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var isAria2cRunning = false
     var statusBar = NSStatusBar.systemStatusBar()
     var statusBarItem : NSStatusItem = NSStatusItem()
+    var statusBarItemSpeed : NSStatusItem = NSStatusItem()
     var menu: NSMenu = NSMenu()
+    var menuSpeed: NSMenu = NSMenu()
     var menuItemExit : NSMenuItem = NSMenuItem()
     var menuItemUI : NSMenuItem = NSMenuItem()
     var menuItemOutput : NSMenuItem = NSMenuItem()
     var menuItemAbout : NSMenuItem = NSMenuItem()
-    
-    
+    var statusUpdateDaemon = NSTimer()
+    var aria2RpcSocket = WebSocket(url: NSURL(string: "http://127.0.0.1:6800/jsonrpc")!)
+    var notiCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
 
     override func awakeFromNib() {
 
         //Add statusBarItem
+        statusBarItemSpeed = statusBar.statusItemWithLength(80)
+        statusBarItemSpeed.menu = menuSpeed
+        statusBarItemSpeed.title = ""
+        
+        
+        //Add statusBarItem
         statusBarItem = statusBar.statusItemWithLength(-1)
         statusBarItem.menu = menu
-        statusBarItem.title = "CuteAria"
+        statusBarItem.image = NSImage(named: "statusIcon.pdf")
+        statusBarItem.image?.setTemplate(true)
+        
+
         
         //Add menuItem to menu
         menuItemUI.title = "Show WebUI"
@@ -66,13 +78,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menuItemExit.keyEquivalent = ""
         menu.addItem(menuItemExit)
         
-        runAria2()
+        killAllAria2()
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+            Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+                self.runAria2()
+        }
+        
+        aria2RpcSocket.delegate = self
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+            Int64(2 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            self.aria2RpcSocket.connect()
+        }
     }
     
     func showAboutDialog(){
         let aboutDialog = NSAlert()
         aboutDialog.messageText = "This program utilizes aria2 downloader binary, which is released under GPLv2. \nLicense and source code avilable at http://aria2.sourceforge.net \nThis program utilizes webui-aria2, which is released under MIT license. \nLicense and source code available at https://github.com/ziahamza/webui-aria2"
         aboutDialog.runModal()
+    }
+    
+    func killAllAria2(){
+        let pkillTask = NSTask()
+        pkillTask.launchPath = "/usr/bin/pkill"
+        pkillTask.arguments = ["aria2c.bin"]
+        pkillTask.launch()
+        pkillTask.waitUntilExit()
+        isAria2cRunning = false
     }
     
     func runAria2(){
@@ -99,12 +131,118 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
+    
+    
+    func updateStatus(){
+        aria2RpcSocket.writeString("{\"jsonrpc\":\"2.0\", \"id\":\"CuteAriaGlobalStat\", \"method\":\"aria2.getGlobalStat\"}")
+    }
+    
+    
+    
+    func updateStatusUI(input: NSDictionary){
+        var text = ""
+        if let numActiveStr = input["numActive"] as? NSString {
+            text = (numActiveStr as String) + "@"
+        }
+        if let downloadSpeedStr = input["downloadSpeed"] as? NSString {
+            let downloadSpeed = downloadSpeedStr.doubleValue
+            var readableSpeed = downloadSpeed
+            var suffix = "B/s"
+            if readableSpeed >= 1000.0 {
+                readableSpeed = readableSpeed / 1024.0
+                suffix = "KB/s"
+                if readableSpeed >= 1000.0 {
+                    readableSpeed = readableSpeed / 1024.0
+                    suffix = "MB/s"
+                }
+            }
+            text = text + String(format:"%.1f", readableSpeed) + suffix
+        }
+        statusBarItemSpeed.title = text
+    }
+    
+    func parseAria2Rpc(text: String){
+        let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(
+            text.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)!,
+            options: NSJSONReadingOptions.AllowFragments,
+            error:nil)
+        
+        if let dict = parsedObject as? NSDictionary {
+            if let id = dict["id"] as? NSString{
+                if id as String == "CuteAriaGlobalStat" {
+                    if let result = dict["result"] as? NSDictionary{
+                        updateStatusUI(result)
+                    }
+                }
+                if id as String == "CuteAriaDownloadComplete" {
+                    if let result = dict["result"] as? NSDictionary{
+                        if let files = result["files"] as? NSArray{
+                            if let file = files[0] as? NSDictionary{
+                                if let filePath = file["path"] as? NSString{
+                                    if files.count > 1 {
+                                        postNoti("Download Completed", subtitle: String(files.count) + "files", informativetext: filePath as String)
+                                    } else if files.count == 1 {
+                                        postNoti("Download Completed", subtitle: "1 file", informativetext: filePath as String)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            if let method = dict["method"] as? NSString{
+                if method == "aria2.onDownloadComplete"{
+                    if let params = dict["params"] as? NSArray{
+                        if let gidContainer = params[0] as? NSDictionary{
+                            if let gid = gidContainer["gid"] as? NSString{
+                                println("complete request sent")
+                                println("{\"jsonrpc\":\"2.0\", \"id\":\"CuteAriaDownloadComplete\", \"method\":\"aria2.tellStatus\", params:[\"" + (gid as String) + "\"]}")
+                                aria2RpcSocket.writeString("{\"jsonrpc\":\"2.0\", \"id\":\"CuteAriaDownloadComplete\", \"method\":\"aria2.tellStatus\", \"params\":[\"" + (gid as String) + "\"]}")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+        statusUpdateDaemon.invalidate()
+        aria2RpcSocket.disconnect()
         terminateAria2()
     }
+    
+    //Websocket callbacks
+    
+    func websocketDidConnect(socket: WebSocket){
+        statusBarItemSpeed.title = "Starting"
+        statusUpdateDaemon = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: Selector("updateStatus"), userInfo: nil, repeats: true)
+    }
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?){
+        
+    }
+    func websocketDidReceiveMessage(socket: WebSocket, text: String){
+        println(text)
+        parseAria2Rpc(text)
+    }
+    func websocketDidReceiveData(socket: WebSocket, data: NSData){
+        
+    }
+    
+    //NOTIFICATION CENTER
 
+    func postNoti(title: String, subtitle: String, informativetext: String){
+        let notification = NSUserNotification()
+        notification.title = title
+        notification.subtitle = subtitle
+        notification.informativeText = informativetext
+        notiCenter.deliverNotification(notification)
+    }
 
+    func shouldPresentNotification(notification: NSUserNotification) -> Bool{
+        return true
+    }
 }
 
